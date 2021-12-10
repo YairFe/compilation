5f91aa94fd3fcaac4f0c4abda8dea08b6fe434f0
catocato <catocato88@gmail.com>
2021-12-10 18:14:52 +0200

    Update TYPE_ARRAY.java

--- ex3/src/TYPES/TYPE_ARRAY.java
+++ ex3/src/TYPES/TYPE_ARRAY.java
@@ -4,7 +4,7 @@ public class TYPE_ARRAY extends TYPE
 {
     public TYPE array_type;
 
-    public TYPE_ARRAY(STRING name, TYPE type){
+    public TYPE_ARRAY(String name, TYPE type){
         this.name = name;
         this.array_type = type;
     }
