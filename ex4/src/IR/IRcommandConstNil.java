703a09e71a21caf7134fb12c4e20e4eee11d2b2b
catocato <catocato88@gmail.com>
2022-01-06 15:19:43 +0200

    Add files via upload

---
+++ ex4/src/IR/IRcommandConstNil.java
@@ -0,0 +1,11 @@
+package IR;
+import TEMP.*;
+
+public class IRcommandConstNil extends IRcommand {
+
+	TEMP t;
+	
+	public IRcommandConstNil(TEMP t) { this.t = t; }
+	public void MIPSme() { /* TODO */	}
+
+}
