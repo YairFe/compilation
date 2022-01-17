16463e48d1516b51f2d645624257e62ff0dca256
catocato <catocato88@gmail.com>
2022-01-17 14:14:59 +0200

    Update REG_ALLOC.java

--- ex4/src/TEMP/REG_ALLOC.java
+++ ex4/src/TEMP/REG_ALLOC.java
@@ -4,12 +4,12 @@ public class REG_ALLOC {
 	private TEMP_LIST all_temporaries = null; // a list of all created temporaries in sorted order
 	
 	private TEMP_LIST stack = null; // will be used as a stack for the graph coloring algorithm
-									// NOTE: adding and removing temporaries from stack should be done
-									// manually and not with add()/remove(), because add() and remove()
-									// maintain a sorted order in the list.
+					// NOTE: adding and removing temporaries from stack should be done
+					// manually and not with add()/remove(), because add() and remove()
+					// maintain a sorted order in the list.
 	
 	private final String[] colors = {"$t0", "$t1", "$t2", "$t3", "$t4", 
-							         "$t5", "$t6", "$t7", "$t8", "$t9"}; // a list of register names to "color" the graph with
+					 "$t5", "$t6", "$t7", "$t8", "$t9"}; // a list of register names to "color" the graph with
 	
 	public void addTemp(TEMP t) {
 		if(this.all_temporaries == null) this.all_temporaries = new TEMP_LIST(t, null);
