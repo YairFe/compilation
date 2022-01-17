74347e876a9cef2d71bda46f41d21152c4a13b03
catocato <catocato88@gmail.com>
2022-01-17 13:41:42 +0200

    update TEMP for use in interference graph

--- ex4/src/TEMP/TEMP.java
+++ ex4/src/TEMP/TEMP.java
@@ -14,6 +14,8 @@ package TEMP;
 public class TEMP
 {
 	private int serial=0;
+	public TEMP_LIST neighbors = null;
+	public String color = null;
 	
 	public TEMP(int serial)
 	{
@@ -25,7 +27,17 @@ public class TEMP
 		return serial;
 	}
 	
+	public void addNeighbor(TEMP n) { 
+		if(this.neighbors == null) this.neighbors = new TEMP_LIST(n, null);
+		else this.neighbors.add(n);
+	}
+	
+	public void removeNeighbor(TEMP n) { 
+		if(this.neighbors != null) this.neighbors.remove(n);
+	}
+	
 	public String toString() {
-		return String.format("Temp_%d", this.serial);
+		if(this.color == null) return String.format("Temp_%d", this.serial);
+		return this.color;
 	}
 }
