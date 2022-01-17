9303248bbb9d0c8630619cb7403e4a179eefefab
catocato <catocato88@gmail.com>
2022-01-17 14:12:08 +0200

    update TEMP for use in interference graph

--- ex4/src/TEMP/TEMP.java
+++ ex4/src/TEMP/TEMP.java
@@ -14,8 +14,8 @@ package TEMP;
 public class TEMP
 {
 	private int serial=0;
-	public TEMP_LIST neighbors = null;
-	public String color = null;
+	public TEMP_LIST neighbors = null; // neighbor list for the interference graph - will be updated during liveness analysis
+	public String color = null; // will contain the register assigned to this temporary variable
 	
 	public TEMP(int serial)
 	{
@@ -28,20 +28,25 @@ public class TEMP
 	}
 	
 	public int numOfNeighbors() {
+		// get current number of interfering temporaries
 		if(this.neighbors == null) return 0;
 		return this.neighbors.length;
 	}
 	
 	public void addNeighbor(TEMP n) { 
+		// add a node to the neighbor list
 		if(this.neighbors == null) this.neighbors = new TEMP_LIST(n, null);
 		else this.neighbors.add(n);
 	}
 	
 	public void removeNeighbor(TEMP n) { 
+		// remove a node from the neighbor list
 		if(this.neighbors != null) this.neighbors.remove(n);
 	}
 	
 	public String toString() {
+		// return a "Temp_n" string if a register was not assigned yet.
+		// If there is an asigned register, return the register.
 		if(this.color == null) return String.format("Temp_%d", this.serial);
 		return this.color;
 	}
