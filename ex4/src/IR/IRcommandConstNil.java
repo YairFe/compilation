21dcdbd037b765ea10971d013f0fabc9ac5fba0b
catocato <catocato88@gmail.com>
2022-01-28 13:59:01 +0200

    debug labels for IR

--- ex4/src/IR/IRcommandConstNil.java
+++ ex4/src/IR/IRcommandConstNil.java
@@ -8,6 +8,7 @@ public class IRcommandConstNil extends IRcommand {
 	
 	public IRcommandConstNil(TEMP t) { this.t = t; }
 	public void MIPSme() { 
+		MIPSGenerator.getInstance().label("IR_ConstNil");
 		MIPSGenerator.getInstance().mov(t.toString(),"$zero");	
 	}
 	public TEMP_LIST getLiveTemp(TEMP_LIST input){
