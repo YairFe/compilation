ac147d5b92e8c291703c95175264ac3771686d2a
catocato <catocato88@gmail.com>
2022-01-31 11:21:31 +0200

    add debug labels

--- ex4/src/IR/IRcommand_Binop_Mul_Integers.java
+++ ex4/src/IR/IRcommand_Binop_Mul_Integers.java
@@ -30,6 +30,7 @@ public class IRcommand_Binop_Mul_Integers extends IRcommand
 	/***************/
 	public void MIPSme()
 	{
+		MIPSGenerator.getInstance().label(String.format("IR_BinopMulInt_%d", this.dst.getSerialNumber()));
 		int max = 32767;
 		int min = -32768;
 		String label_end_max = getFreshLabel("label_end_max");
